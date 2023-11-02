3a22708b6336c626822bbfa4529032b31369abfd
Nicolas CUVÉ <nicolascuve.dev@gmail.com>
2023-11-02 20:13:29 +0100

    morgan + winston logger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import morgan from 'morgan';
 
 const PORT: string | number = 3333 || process.env.PORT;
 const HOST: string = process.env.HOST || 'localhost';
@@ -15,6 +16,7 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  app.use(morgan('common'));
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Magic Inn')
     .setDescription('API for Magic Inn')
